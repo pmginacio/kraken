f70cde9ae05dce0b24507846692a3f4eab5a34ef
Pedro Inácio <pedromiragaia@gmail.com>
2020-10-23 12:31:31 +0200

    fixed book update algorithm and implemented CRC32 check

--- kraken.py
+++ kraken.py
@@ -34,9 +34,13 @@
 # allow offloading the message processing part to other cores, and/or add 
 # multiple coroutines to process the incoming messages.
 #
+# I did not put effort in additional functionalities or make nice output.
 # I did not put significant amount of effort in handling and mitigating
-# possible errors and exceptions. I did not put effort in additional
-# functionalities or make nice output.
+# possible errors and exceptions. Particularly, if there is a checksum error,
+# the program will raise a ValueError exception and stop. As far as I can tell,
+# the book update algorightm is working correctly and this should not happen. 
+# On the other hand, to make it more robust, one idea would be to resubscribe
+# once a checksum becomes invalid and carry on.
 #
 # While I was developing this application, I kept in mind that ultimately 
 # we would like to connect and digest messages from multiple exchanges.
@@ -50,11 +54,10 @@
 # the global queue.
 #
 # Possible misunderstandings from my side:
-#   - price update only issued when new ask/bid were lower/higher than the 
-#     lowest/highest ask/bid in the top ten 10. I'm not sure this makes sense
-#     since then updates will become less and less frequent?
-#   - i was not sure which timestamp to use in the output or whether I should 
-#     generate one. therefore I just set it to None.
+#   - with an update there is may be a new bid or a new ask. if there is any of 
+#     those I update the last_bid or last_ask in the Asset object and I also update
+#     the timestamp. So in the update output, the published timestamp refers to 
+#     either one of the last bid or ask.
 #   - i assumed that the concept of asset, which I believe is a (coin/market) pair 
 #     (e.g. 'ETH/JPY') is a relevant concept to hold on to. that is why i created
 #     an Asset() class.
@@ -62,6 +65,9 @@
 import asyncio
 import websockets as ws
 import json
+from zlib import crc32
+from bisect import bisect
+from copy import deepcopy
 
 # this is used to fetch the list of assets through the REST API
 import requests as rq
@@ -117,14 +123,16 @@ class KRKXCH(object):
         if None, all the assets in the exchange will be subscribed
         '''
 
-        # use all assests if None was specified
+        aux = self.get_assets()
         if lassets is None:
-            lassets = self.get_assets()
+            # use all availanle assests if None was specified
+            lassets = aux
         else:
             # check if requested assets are valid
             for x in lassets:
-                if x not in self.lassets:
-                    raise ValueError('%x not available in %s exchange' % (x,self.NAME))
+                if x not in aux:
+                    raise ValueError('%s not available in %s exchange' % (
+                        x,self.NAME))
 
         # return JSON payload
         return json.dumps({
@@ -151,7 +159,7 @@ class KRKXCH(object):
             
             # subscribe to assets
             await krk.send(self.get_subscribe_payload())
-
+            
             # listen to the subscriptions
             while True:
                 upd = await krk.recv()  
@@ -170,9 +178,26 @@ class KRKXCH(object):
             upd = json.loads(upd)
 
             # drop everything except
-            #   snapshot sent once when subscribed
-            #   update sent afterwards
-            # both are lists with a dictionary in the 1st index
+            #   - snapshot sent once when subscribed: a list with a dictionary 
+            #       with fields 'as' and 'bs' in the 1st index
+            #   - update sent afterwards: similar, with one or more dictionaries,
+            #       with fields, at least one of a or b and c. two valid examples 
+            #       follow,
+            #       
+            #       [360, {
+            #           'a': [['56.25000', '81.92801619', '1603445162.315265']]
+            #       },
+            #       {
+            #           'b': [['56.25000', '0.00000000', '1603445162.318280'], ['56.11000', '90.60000000', '1603445162.113424', 'r']], 
+            #           'c': '1810513080'
+            #       }, 'book-10', 'LTC/USD']
+            #       or 
+            #       [360, {
+            #           'a': [['56.25000', '81.92801619', '1603445162.315265']], 
+            #           'b': [['56.25000', '0.00000000', '1603445162.318280'], ['56.11000', '90.60000000', '1603445162.113424', 'r']], 
+            #           'c': '1810513080'
+            #       }, 'book-10', 'LTC/USD']
+            #   
             if isinstance(upd,list) and isinstance(upd[1],dict):
 
                 # get asset name
@@ -187,12 +212,29 @@ class KRKXCH(object):
                         self.uq)
                     continue
 
-                elif len(upd) == 4 and any(x in upd[1] for x in ['a','b']):
-                    # 
-                    newask = upd[1].pop('a',None)
-                    newbid = upd[1].pop('b',None)
-                    # logger.debug('sending update: ', upd)
-                    await self.lassets[asset_name].update(self.NAME,newask,newbid)
+                elif len(upd) >= 4 and any(x in upd[1] for x in ['a','b']):
+
+                    # logger.debug('upd:',upd)
+
+                    # init
+                    na = None
+                    nb = None
+                    cs = None
+
+                    # loop up to two dictionaries and accumulate 
+                    # possibly present keys
+                    for x in upd[1:3]:
+                        if isinstance(x,dict):
+                            na = x.pop('a',na)
+                            nb = x.pop('b',nb)
+                            cs = x.pop('c',cs)
+
+                    # if na is not None and nb is not None:
+                    #     logger.debug('na:',na)
+                    #     logger.debug('nb:',nb)
+                    #     logger.debug('nc:',cs)
+
+                    await self.lassets[asset_name].update(self.NAME,na,nb,cs)
                     continue
 
             # logger.debug('dropped', upd)
@@ -220,18 +262,19 @@ class Asset(object):
         self.n = n
         self.coin,self.market = name.split('/')
 
-        # type conversion, only keep price and volume
-        self.lask = [[float(x) for x in y[0:2]] for y in ba]
-        self.lbid = [[float(x) for x in y[0:2]] for y in bs]
+        # keep price and volume
+        self.lask = [[x for x in y[0:2]] for y in ba]
+        self.lbid = [[x for x in y[0:2]] for y in bs]
 
         self.last_ask = None
         self.last_bid = None
-        # timestamp, not being used 
+
         self.ts = None
 
+        # external price update queue
         self.uq = uq
 
-    async def update(self, xch, na, nb):
+    async def update(self, xch, na, nb, cs):
         '''
         update this asset
         xch is the name of the exchange pushing an update
@@ -255,83 +298,160 @@ class Asset(object):
         self.ts]        # timestamp
         '''
 
-        # dont publish unless price changed
-        publish = False
-
+        # *_before and *_after commented code was used for debug
+        # may be removed after validation
         if na is not None:
-            # na and nb sometimes are [a,b,c] and sometimes [[a,b,c],[d,e,f],...]
-            # if [a,b,c], then convert to [[a,b,c]]
-            # same below for nb
-            if not isinstance(na[0],list):
-                na = [na]
-            # convert to float, only keep price and volume
-            na = [[float(x) for x in y[0:2]] for y in na]
 
-            # save current top ask, if it changes an update is triggered
-            old_top_ask = self.lask[0]
+            # lask_before=[x.copy() for x in self.lask]
+                       
+            publish = self._update_book(na,'ask')
 
-            # store last ask price
-            self.last_ask = na[-1][0]
-
-            # append and filter out zero volume 
-            tmp = self.lask +list(filter(lambda x: x[1]>0, na))
-            # sort list ascending
-            tmp.sort()
-            # get a unique list of prices
-            pset = set([x[0] for x in tmp])
-            # and remove duplicate prices from tmp
-            self.lask = []
-            for x in tmp:
-                if x[0] in pset:
-                    self.lask.append(x)
-                    pset.remove(x[0])
-            # keep at most n
-            self.lask=self.lask[0:self.n]
-
-            # if the top ask changed, publish update
-            if self.lask[0] != old_top_ask:
-                publish = True
-                    
+            # lask_after=[x.copy() for x in self.lask]
+                 
         if nb is not None:
-            if not isinstance(nb[0],list):
-                nb = [nb]
 
-            # convert to float, keep only price and volume
-            nb = [[float(x) for x in y[0:2]] for y in nb]
+            # lbid_before=[x.copy() for x in self.lbid]
 
-            # save current top bid , if it changes an update is triggered
-            old_top_bid = self.lbid[0]
+            publish = self._update_book(nb,'bid')
 
-            # store last bid
-            self.last_bid = nb[-1][0]
+            # lbid_after=[x.copy() for x in self.lbid]
 
-            # append, sort descending price and ascending volume
-            tmp = self.lbid +list(filter(lambda x:x[1]>0, nb))
-            # sort list price descending, volume ascending
-            tmp.sort(key=lambda x: (x[0], -x[1]), reverse=True)
-            # get a unique list of prices
-            pset = set([x[0] for x in tmp])
-            # and remove duplicate prices from tmp
-            self.lbid = []
-            for x in tmp:
-                if x[0] in pset:
-                    self.lbid.append(x)
-                    pset.remove(x[0])
-            # keep at most n
-            self.lbid=self.lbid[0:self.n]
+        if cs is not None and not self._check(cs):
 
-            # publish only if the best bid changed
-            if self.lbid[0] != old_top_bid:
-                publish = True
+            # print(self.coin,self.market)
+            # if na is not None:
+            #     logger.debug(' lask before:')
+            #     for x in lask_before:
+            #         print(x)
+            #     logger.debug(' na=',na)
+            #     logger.debug(' lask after:')
+            #     for x in lask_after:
+            #         print(x)
+
+            # if nb is not None:
+            #     logger.debug(' lbid before:')
+            #     for x in lbid_before:
+            #         print(x)
+            #     logger.debug(' nb=',nb)
+            #     logger.debug(' lbid after:')
+            #     for x in lbid_after:
+            #         print(x)
+            
+            # it's not a bad idea to resbuscribe when something goes wrong
+            # i avoided doing that because it is important to get the book 
+            # update algorithm working correctly
+            raise ValueError('incorrect checksum')
 
         if publish:
             await self.uq.put([xch, self.coin, self.market, self.lask,
                 self.lbid, self.last_ask, self.last_bid, self.ts])
-        # #DEBUG
-        # else:
-        #     logger.debug('got update, no price change: ', 
-        #         [xch, self.coin, self.market, self.last_ask, self.last_bid])
 
+
+    def _check(self,cs):
+        '''
+        compute the CRC32 checksum of the current book and compare it with
+        the input cs
+        https://docs.kraken.com/websockets/#book-checksum
+        return True if they match and False otherwise
+        '''
+
+        # only first ten are used
+        n=10
+
+        ss = ''
+        # loop asks and then bids
+        for z in [self.lask[0:n], self.lbid[0:n]]:
+            # flatten list of lists
+            tmp = [x for y in z for x in y]
+            # remove decimal and leading zeros and concatenate
+            ss += ''.join(map(lambda x: x.replace('.','').lstrip('0'),tmp))
+
+        # return test for match
+        return int(cs) == crc32(ss.encode())
+
+    def _update_book(self,n,mode):
+        '''
+        update book with new ask/bid n.
+        mode can be 'ask' or 'bid'
+
+        function will modify self.lask or self.lbid according to doc
+        https://support.kraken.com/hc/en-us/articles/360027821131-How-to-maintain-a-valid-order-book-
+
+        returns True if an update should be published and False otherwise
+        '''
+
+        # check input
+        if mode not in ['ask','bid']:
+            raise ValueError('incorrect mode')
+
+        # init
+        # only publish if top bid/ask price changed
+        publish = False 
+        # dont change n in the calling function
+        n_ = deepcopy(n)
+        # select which list to update depending on the mode
+        last = None
+        if mode == 'ask':
+            out = self.lask
+        else:
+            out = self.lbid
+
+        # loop input n and consume asks/bids in it
+        while len(n) > 0:
+
+            # split price value pair
+            p,v,t = n.pop(0)[0:3]
+
+            # check if price level already in list
+            try:
+                idx=[x[0] for x in out].index(p)
+            except ValueError:
+                # price not in top asks
+                idx = None
+
+            if idx is None:
+
+                # discard if volume is 0 and not on the list
+                if float(v) == 0:
+                    continue
+
+                # new price level, find correct index to insert
+                if mode =='ask':
+                    idx = bisect([float(x[0]) for x in out],float(p))
+                else:
+                    idx = bisect([-float(x[0]) for x in out],-float(p))
+
+                # insert at the right place
+                out.insert(idx,[p,v])
+
+                # pop last item if list is longer than self.n
+                if len(out) > self.n:
+                    out.pop()
+
+                # publish if top ask/bid 
+                if idx == 0:
+                    publish = True
+            else:
+                if float(v) == 0:
+                    # delete price level
+                    out.pop(idx)
+
+                    # publish if top price level was deleted
+                    if idx == 0:
+                        publish = True
+                else:
+                    # update volume for existing price level
+                    out[idx][1] = v
+
+            # keep last price level only if volume is not zero
+            if float(v) != 0:
+                self.ts = t
+                if mode == 'ask':
+                    self.last_ask = p
+                else:
+                    self.last_bid = p
+
+        return publish
 
 async def worker(q):
     '''
